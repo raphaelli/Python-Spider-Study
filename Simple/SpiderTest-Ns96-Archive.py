e482a93e48033b8eb2aa7a63ac37ff32bba8778c
raphaelli <raphael_li@live.com>
2018-01-23 19:08:24 +0800

    fix bug

--- Simple/SpiderTest-Ns96-Archive.py
+++ Simple/SpiderTest-Ns96-Archive.py
@@ -5,7 +5,6 @@ import os
 import requests
 from bs4 import BeautifulSoup
 
-
 headers = {'User-Agent':"Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.1(KHTML, like Gecko) Chrome/22.0.1207.1 Safari/537.1"}
 target_Url = "https://ns96.com/archives/"
 ext_html = requests.get(target_Url,headers=headers) 
@@ -13,7 +12,5 @@ ext_html = requests.get(target_Url,headers=headers)
 soup = BeautifulSoup(ext_html.text,'lxml')
 urls = soup.select('a.archive-title')
 
-
 for link in urls:
-    print("https://"+link.get('href'))
-
+    print("https://www.ns96.com"+link.get('href'))
